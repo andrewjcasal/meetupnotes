315363d9d25ac9013ed4774ab4e2f3994b069106
Jason Pearson <jason.d.pearson@gmail.com>
2012-07-21 19:34:38 -0400

    Merged schema.sql, fixed notes.php's add note method.

--- schema.sql
+++ schema.sql
@@ -1,10 +1,6 @@
 CREATE TABLE `meetupnotes`.`notes` (
   `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-<<<<<<< HEAD
-  `event_id` INTEGER UNSIGNED NULL,
-=======
   `event_id` VARCHAR(255) NULL,
->>>>>>> faac10163b15ef1fc0053bc81d4a27169f68a397
   `user_id` INTEGER UNSIGNED NULL,
   `title` TEXT NULL,
   `description` TEXT NULL,
